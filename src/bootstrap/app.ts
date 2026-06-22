b3e822ccefa8a81307745008fe84b9823103eae3
Naveenkumar153 <naveenkumartech2025@gmail.com>
2026-06-22 23:38:17 +0530

    feat: Added the cors package to handle the origin

--- src/bootstrap/app.ts
+++ src/bootstrap/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { errorHandler } from '@/handlers/error.handlers.js';
 import { authMiddleware } from '@/shared/middleware/auth.middleware.js';
 import { authRoutes } from '@/modules/auth/auth.index.js';
@@ -11,11 +12,13 @@ const app: express.Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use('/api', authRoutes);
 app.use('/api', authMiddleware.checkToken, feedRouters);
 app.use('/api', authMiddleware.checkToken, usersRouters);
 app.use('/api', authMiddleware.checkToken, connectionRouters);
 app.use(errorHandler);
-
-
 export { app };
