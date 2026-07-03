0c192c7d89bfff65b53f2ecd8c7f0cb354113b3e
Naveenkumar153 <naveenkumartech2025@gmail.com>
2026-07-03 22:04:21 +0530

    feat: Update database connection string and enhance build process with tsc-alias

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-   return await mongoose.connect("mongodb+srv://naveen:naveennaveen@node-project.ijcdmqt.mongodb.net/DevTinder") as mongoose.Mongoose;
+    return await mongoose.connect("mongodb+srv://naveen:E6saexP2UyOnDDAR@node-project.ijcdmqt.mongodb.net/DevTinder") as mongoose.Mongoose;
 };
 
 
